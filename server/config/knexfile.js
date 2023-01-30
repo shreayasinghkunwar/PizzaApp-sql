078b71e372e4fd308c9f190a7dc745f28e492cf5
RatishNyaichyai <ratish@fusemachines.com>
2023-01-30 17:59:21 +0545

    test

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 console.log(process.env.DATABASE_CONNECTION_STRING)
 module.exports = {
     client: "pg",
-    connection: "postgresql://postgres:frznheart20@localhost:5433/food"
+    // connection: "postgresql://postgres:frznheart20@localhost:5433/food"
+    connection: "postgres://tzazqmmm:naPG-R95ZDvlQX5ToLgO0EGe98rrvYSN@satao.db.elephantsql.com/tzazqmmm"
 };
